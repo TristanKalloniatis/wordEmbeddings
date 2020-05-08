d38d848956f72b35806624856af0a92d22d3f5a4
TristanKalloniatis <tristankalloniatis@gmail.com>
2020-05-08 13:07:37 +0100

    First version of RNN to try and predict overall score

--- RNNForScoring.py
+++ RNNForScoring.py
@@ -5,7 +5,7 @@ from json import loads
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.optim import SGD
+from torch.optim import Adam
 from datetime import datetime
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import matplotlib.pyplot as plt
@@ -20,25 +20,27 @@ EMBEDDING_DIM = 16
 TRAIN_PROPORTION = 0.75
 VALID_PROPORTION = 0.15
 LEARNING_RATE = 0.005
-MOMENTUM = 0.9
-BATCH_SIZE = 1000
-EPOCHS = 200
+BATCH_SIZE = 1024
+EPOCHS = 50  # todo: probably increase this once it looks like something is happening
 LEARNING_RATE_DECAY_FACTOR = 0.5
-PATIENCE = 5
+PATIENCE = 2
 UNIGRAM_DISTRIBUTION_POWER = 0.75
 UNKNOWN_TOKEN = '???'
-HIDDEN_STATE_CLAMP = 10
+HIDDEN_STATE_CLAMP = 4
 EXPLOSION_DETECTION_FACTOR = 3
 MAX_EXPLOSIONS = 2
-NUM_HIDDEN_STATES = 100
+NUM_HIDDEN_STATES = 10
 NUM_CATEGORIES = 2
+MAX_REVIEW_LENGTH = 200
+NUM_BATCHES_TO_PLOT = 1
+BATCHES = EPOCHS * NUM_BATCHES_TO_PLOT
 
 REVIEW_FILE = 'reviews_Grocery_and_Gourmet_Food_5.json.gz'
 IMPLEMENTED_MODELS = ['CBOW', 'SGNS']
 MIN_REVIEW_LENGTH = 2 * CONTEXT_SIZE + 1
-CUDA = torch.cuda.is_available()
-FULL_NAME = 'RNN' + 'HSC' + str(HIDDEN_STATE_CLAMP) + 'EDF' + str(EXPLOSION_DETECTION_FACTOR) + 'ME' + \
-            str(MAX_EXPLOSIONS) + 'NHS' + str(NUM_HIDDEN_STATES) + 'NC' + str(NUM_CATEGORIES) + ALGORITHM_TYPE
+FULL_NAME = 'RNN_HSC{0}_EDF{1}_ME{2}_NHS{3}_NC{4}_MRL{5}_{6}'.format(HIDDEN_STATE_CLAMP, EXPLOSION_DETECTION_FACTOR,
+                                                                     MAX_EXPLOSIONS, NUM_HIDDEN_STATES, NUM_CATEGORIES,
+                                                                     MAX_REVIEW_LENGTH, ALGORITHM_TYPE)
 
 
 def noiseDistribution(frequencies, unigramDistributionPower):
@@ -145,34 +147,46 @@ def loadModelState(modelName, logObject, algorithm=ALGORITHM_TYPE, unigramDistri
         model = SkipGramWithNegativeSampling(len(vocabulary), modelData['embeddingDim'], modelData['contextSize'],
                                              modelData['numNegativeSamples'], modelData['innerProductClamp'], modelName)
     model.load_state_dict(torch.load(modelName + algorithm + '.pt'))
-    if CUDA:
-        model.cuda()
     writeLog("Loaded model {0}".format(modelName), logObject)
     model.eval()
     return wordMapping, reverseWordMapping, vocabulary, frequencies, distribution, model
 
 
-def writeLog(message, logObject, timestamp=datetime.now()):
+def writeLog(message, logObject):
+    timestamp = datetime.now()
     logObject.info("[{0}]: {1}".format(str(timestamp), message))
     return
 
 
 class RNN(nn.Module):
-    def __init__(self, inputSize, hiddenSize, outputSize):
+    def __init__(self, inputSize, hiddenSize, outputSize, learningRate):
         super().__init__()
         self.hiddenSize = hiddenSize
         self.inputSize = inputSize
         self.outputSize = outputSize
         self.toHidden = nn.Linear(inputSize + hiddenSize, hiddenSize)
-        self.toOutput = nn.Linear(inputSize + hiddenSize, outputSize)
+        self.toOutput = nn.Linear(hiddenSize, outputSize)
+        self.trainLosses = []
+        self.validLosses = []
+        self.trainingTimes = []
+        self.initialLearningRate = learningRate
 
     def forward(self, inputs):
         hidden = torch.zeros(self.hiddenSize)
+        numWordsProcessed = 0
         for word in inputs:
             combined = torch.cat((word, hidden), dim=0)
             hidden = torch.clamp(self.toHidden(combined), min=-HIDDEN_STATE_CLAMP, max=HIDDEN_STATE_CLAMP)
-            preOutput = self.toOutput(combined)
+            hidden = torch.tanh(hidden)
+            numWordsProcessed += 1
+            if numWordsProcessed >= MAX_REVIEW_LENGTH:
+                break
+        preOutput = torch.clamp(self.toOutput(hidden), min=-HIDDEN_STATE_CLAMP, max=HIDDEN_STATE_CLAMP)
         return F.log_softmax(preOutput, dim=-1)
+
+    @property
+    def numBatchesTrained(self):
+        return len(self.trainLosses)
 
 
 def wordToTensor(word, vocabSize):
@@ -190,7 +204,7 @@ def randomTrainingExample(reviews, wordMapping, vocabSize, embeddingModel=None):
     reviewLength = 0
     while reviewLength < MIN_REVIEW_LENGTH:
         review = randomChoice(reviews)
-        if 'overall' not in review:
+        if 'overall' not in review or 'reviewText' not in review:
             continue
         if review['overall'] not in [1.0, 2.0, 3.0, 4.0, 5.0]:
             continue
@@ -200,17 +214,12 @@ def randomTrainingExample(reviews, wordMapping, vocabSize, embeddingModel=None):
         targetOutput = 1
     else:
         targetOutput = 0
-    inputWords = []
     if not embeddingModel:
-        for word in words:
-            wordEmbedding = wordToTensor(word, vocabSize)
-            inputWords.append(wordEmbedding)
+        inputWords = [wordToTensor(word, vocabSize) for word in words]
     else:
         with torch.no_grad():
-            for word in words:
-                wordTensor = torch.tensor(wordMapping[word.lower()], dtype=torch.long)
-                wordEmbedding = embeddingModel.embeddings(wordTensor)
-                inputWords.append(wordEmbedding)
+            inputWords = [embeddingModel.embeddings(torch.tensor(wordMapping[word.lower()], dtype=torch.long)) for word
+                          in words]
     return inputWords, targetOutput
 
 
@@ -244,86 +253,105 @@ def validateOnBatch(rnn, reviews, wordMapping, vocabSize, batchSize=BATCH_SIZE, 
     return batchLoss.item()
 
 
-def trainRNN(modelName, epochs, trainData, validData, algorithm, wordMapping, vocabSize, logObject, embeddingModel=None,
+def trainRNN(modelName, trainData, validData, algorithm, wordMapping, vocabSize, logObject, embeddingModel=None,
              embeddingDim=EMBEDDING_DIM, contextSize=CONTEXT_SIZE, numHiddenStates=NUM_HIDDEN_STATES,
-             numCategories=NUM_CATEGORIES, batchSize=BATCH_SIZE, lr=LEARNING_RATE, momentum=MOMENTUM,
-             learningRateDecayFactor=LEARNING_RATE_DECAY_FACTOR, patience=PATIENCE,
+             numCategories=NUM_CATEGORIES, batchSize=BATCH_SIZE, lr=LEARNING_RATE,
+             learningRateDecayFactor=LEARNING_RATE_DECAY_FACTOR, patience=PATIENCE, batches=BATCHES,
              explosionDetectionFactor=EXPLOSION_DETECTION_FACTOR, maxExplosions=MAX_EXPLOSIONS):
     if algorithm:
-        rnn = RNN(embeddingDim, numHiddenStates, numCategories)
+        rnn = RNN(embeddingDim, numHiddenStates, numCategories, lr)
     else:
-        rnn = RNN(vocabSize, numHiddenStates, numCategories)
-    rnnOptimiser = SGD(rnn.parameters(), lr=lr, momentum=momentum, nesterov=True)
+        rnn = RNN(vocabSize, numHiddenStates, numCategories, lr)
+    rnnOptimiser = Adam(rnn.parameters(), lr=lr)
     rnnScheduler = ReduceLROnPlateau(rnnOptimiser, mode='min', factor=learningRateDecayFactor, patience=patience,
                                      verbose=True)
-    trainLosses = []
-    validLosses = []
-    epoch = 0
-    lastEpochExploded = False
-    while epoch < epochs:
-        if not lastEpochExploded:
-            numExplosionsThisEpoch = 0
+    trainLossesByBatch = []
+    validLossesByBatch = []
+    trainLossesByEpoch = []
+    validLossesByEpoch = []
+    batch = 0
+    lastBatchExploded = False
+    trainLossThisEpoch = 0
+    validLossThisEpoch = 0
+    while batch < batches:
+        if not lastBatchExploded:
+            numExplosionsThisBatch = 0
         now = datetime.now()
-        writeLog("Running epoch {0}".format(epoch), logObject)
+        writeLog("Running batch {0}".format(batch), logObject)
         rnnPreExplosion = rnn
         trainLoss = trainOnBatch(rnn, trainData, rnnOptimiser, wordMapping, vocabSize, batchSize, embeddingModel)
         validLoss = validateOnBatch(rnn, validData, wordMapping, vocabSize, batchSize, embeddingModel)
         seconds = (datetime.now() - now).total_seconds()
-        writeLog("Epoch took: {0} seconds".format(str(seconds)), logObject)
-        if epoch > 0:
-            if trainLoss > explosionDetectionFactor * trainLosses[epoch - 1] \
-                    or validLoss > explosionDetectionFactor * validLosses[epoch - 1]:
-                writeLog("Losses are exploding, aborting epoch", logObject)
-                numExplosionsThisEpoch += 1
-                lastEpochExploded = True
+        rnn.trainingTimes.append(seconds)
+        writeLog("Batch took: {0} seconds".format(str(seconds)), logObject)
+        if batch > 0:
+            if trainLoss > explosionDetectionFactor * trainLossesByBatch[batch - 1] \
+                    or validLoss > explosionDetectionFactor * validLossesByBatch[batch - 1]:
+                writeLog("Losses are exploding, aborting batch", logObject)
+                numExplosionsThisBatch += 1
+                lastBatchExploded = True
                 rnn = rnnPreExplosion
-                if numExplosionsThisEpoch > maxExplosions:
-                    writeLog("Exceeded the maximum number of explosions this epoch, aborting training", logObject)
+                if numExplosionsThisBatch > maxExplosions:
+                    writeLog("Exceeded the maximum number of explosions this batch, aborting training", logObject)
                     fig, ax = plt.subplots()
-                    ax.plot(range(epoch), trainLosses, label="Training")
-                    ax.plot(range(epoch), validLosses, label="Validation")
-                    ax.set_xlabel("Epoch")
+                    ax.plot(range(batch), trainLossesByBatch, label="Training")
+                    ax.plot(range(batch), validLossesByBatch, label="Validation")
+                    ax.set_xlabel("Batch")
                     ax.set_ylabel("Loss")
                     ax.set_title("Learning curve for RNN model {0}".format(modelName))
                     ax.legend()
                     plt.savefig(
                         '{0}learningCurveRNN{1}{2}{3}.png'.format(modelName, embeddingDim, algorithm, contextSize))
-                    return rnn
+                    return rnn, trainLossesByEpoch, validLossesByEpoch
             else:
-                trainLosses.append(trainLoss)
-                validLosses.append(validLoss)
+                trainLossesByBatch.append(trainLoss)
+                validLossesByBatch.append(validLoss)
+                trainLossThisEpoch += trainLoss
+                validLossThisEpoch += validLoss
+                rnn.trainLosses.append(trainLoss)
+                rnn.validLosses.append(validLoss)
                 writeLog("Training loss: {0}".format(trainLoss), logObject)
                 writeLog("Validation loss: {0}".format(validLoss), logObject)
-                epoch += 1
-                lastEpochExploded = False
-                rnnScheduler.step(validLoss)
+                batch += 1
+                lastBatchExploded = False
+                if batch % NUM_BATCHES_TO_PLOT == 0:
+                    trainLossThisEpoch /= NUM_BATCHES_TO_PLOT
+                    validLossThisEpoch /= NUM_BATCHES_TO_PLOT
+                    trainLossesByEpoch.append(trainLossThisEpoch)
+                    validLossesByEpoch.append(validLossThisEpoch)
+                    writeLog("Average training loss over epoch {0} was {1}".format(len(trainLossesByEpoch),
+                                                                                   trainLossThisEpoch),
+                             logObject)
+                    writeLog("Average validation loss over epoch {0} was {1}".format(len(validLossesByEpoch),
+                                                                                     validLossThisEpoch),
+                             logObject)
+                    rnnScheduler.step(trainLossThisEpoch)  # todo: change this back to validLossThisEpoch
+                    trainLossThisEpoch = 0
+                    validLossThisEpoch = 0
         else:
-            trainLosses.append(trainLoss)
-            validLosses.append(validLoss)
+            trainLossesByBatch.append(trainLoss)
+            validLossesByBatch.append(validLoss)
+            trainLossThisEpoch += trainLoss
+            validLossThisEpoch += validLoss
             writeLog("Training loss: {0}".format(trainLoss), logObject)
             writeLog("Validation loss: {0}".format(validLoss), logObject)
-            epoch += 1
-            lastEpochExploded = False
-            rnnScheduler.step(validLoss)
-    fig, ax = plt.subplots()
-    ax.plot(range(epochs), trainLosses, label="Training")
-    ax.plot(range(epochs), validLosses, label="Validation")
-    ax.set_xlabel("Epoch")
+            batch += 1
+            lastBatchExploded = False
+    _, ax = plt.subplots()
+    ax.plot(range(batches), trainLossesByBatch, label="Training")
+    ax.plot(range(batches), validLossesByBatch, label="Validation")
+    ax.set_xlabel("Batch")
     ax.set_ylabel("Loss")
     ax.set_title("Learning curve for RNN model {0}".format(modelName))
     ax.legend()
     plt.savefig('{0}learningCurveRNN{1}{2}{3}.png'.format(modelName, embeddingDim, algorithm, contextSize))
-    return rnn
+    return rnn, trainLossesByEpoch, validLossesByEpoch
 
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, stream=stdout)
 logger.addHandler(logging.FileHandler("log" + FULL_NAME + ".txt"))
 writeLog("Running {0}".format(FULL_NAME), logger)
-if CUDA:
-    writeLog("Cuda is available", logger)
-else:
-    writeLog("Cuda is not available", logger)
 
 wordIndex, reverseWordIndex, vocab, wordFrequencies, sampleDistribution, loadedModel = loadModelState(NAME, logger,
                                                                                                       algorithm=
@@ -332,9 +360,62 @@ if not ALGORITHM_TYPE:
     loadedModel = None
 allRawData = getData(REVIEW_FILE, logger)
 trainRawData, validRawData, testRawData = splitData(allRawData, TRAIN_PROPORTION, VALID_PROPORTION)
+trainRawData = trainRawData[:NUM_BATCHES_TO_PLOT * BATCH_SIZE]
+validRawData = validRawData[:NUM_BATCHES_TO_PLOT * BATCH_SIZE]
 writeLog("{0} training reviews, {1} validation reviews, {2} testing reviews".format(len(trainRawData),
                                                                                     len(validRawData),
                                                                                     len(testRawData)),
          logger)
+rnnModel, trainByEpoch, validByEpoch = trainRNN(NAME, trainRawData, validRawData, ALGORITHM_TYPE, wordIndex, len(vocab),
+                                                logger, embeddingModel=loadedModel)
+writeLog("Training losses by epoch:", logger)
+for loss in trainByEpoch:
+    writeLog(str(loss), logger)
+writeLog("Validation losses by epoch:", logger)
+for loss in validByEpoch:
+    writeLog(str(loss), logger)
+
+
+_, axes = plt.subplots()
+axes.plot(range(EPOCHS - 1), trainByEpoch, label="Training")
+axes.plot(range(EPOCHS - 1), validByEpoch, label="Validation")
+axes.set_xlabel("Epoch")
+axes.set_ylabel("Average loss")
+axes.set_title("Learning curve for RNN model by epoch")
+axes.legend()
+
+
+def accuracy(data, rnn, logObject):
+    preds = []
+    actuals = []
+    numCorrect = 0
+    for review in data:
+        words = preProcess(review['reviewText']).split()
+        if len(words) < MIN_REVIEW_LENGTH:
+            continue
+        if 'overall' not in review:
+            continue
+        if review['overall'] == 5.0:
+            actuals.append(1)
+        else:
+            actuals.append(0)
+        with torch.no_grad():
+            inputWords = [loadedModel.embeddings(torch.tensor(wordIndex[word.lower()], dtype=torch.long)) for word in
+                          words]
+            rnnOutput = rnn(inputWords)
+            preds.append(torch.argmax(rnnOutput).item())
+    writeLog("Number predicted: {0}, number actual: {1} (out of {2})".format(sum(preds), sum(actuals), len(actuals)),
+             logObject)
+    for i in range(len(actuals)):
+        if preds[i] == actuals[i]:
+            numCorrect += 1
+    writeLog("Proportion correct: {0}".format(numCorrect / len(actuals)), logObject)
+    return
+
+
+writeLog("Train data accuracy", logger)
+accuracy(trainRawData, rnnModel, logger)
+writeLog("Valid data accuracy", logger)
+accuracy(validRawData, rnnModel, logger)
 
 writeLog("Finished running {0}".format(FULL_NAME), logger)
